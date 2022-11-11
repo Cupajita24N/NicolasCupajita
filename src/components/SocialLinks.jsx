542d7fde0770cffd5519e98734de63cd72939430
Cupajita24N <nicolascupajita123@gmail.com>
2022-11-11 16:49:59 -0500

    linkSocial

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,21 +4,70 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 
 export const SocialLinks = () => {
+
+    const links = [
+        {
+            id: 1,
+            child: (
+                <>
+                    LinkedIn <FaLinkedin size={30}/>
+                </>
+            ),
+            href: 'https://www.linkedin.com/in/nicolas-cupajita-b062211a7/',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                    GitHub <FaGithub size={30}/>
+                </>
+            ),
+            href: 'https://github.com/Cupajita24N',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 3,
+            child: (
+                <>
+                    Mail <HiOutlineMail size={30}/>
+                </>
+            ),
+            href: 'mailto:foo@gmail.com',
+        },
+        {
+            id: 4,
+            child: (
+                <>
+                    Home <BsFillPersonLinesFill size={30}/>
+                </>
+            ),
+            href: '/',
+            style: 'rounded-tr-md'
+        },
+
+    ]
+
   return (
-    <div className='flex-col top-[35%] left-0 fixed'>
+    <div className='flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            <li className='flex justify-between items-center w-40 h-14
+
+            {links.map((id, child, href, style) => (
+                <li key={id} className='flex justify-between items-center w-40 h-14
                 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300
                 bg-gray-500'>
-                <a 
-                    href='/' className='flex justify-between items-center w-full
-                                      text-white'
-                >
-                    <>
-                      LinkedIn <FaLinkedin size={30}/>
-                    </>
-                </a>
-            </li>
+                    <a 
+                        href='/' className='flex justify-between items-center w-full
+                                        text-white'
+                    >
+                        <>
+                        LinkedIn <FaLinkedin size={30}/>
+                        </>
+                    </a>
+                </li>
+            ))}
+
+           
         </ul>
     </div>
   )
